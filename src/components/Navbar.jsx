e9957991775c2c8bac3c66feab64ea090cc8d0dc
Shivansh Srivastava <shivanshsri.04@gmail.com>
2025-05-18 10:13:03 +0530

    style: nabar actions removed for small screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ export default function Navbar() {
     return (
         <div className="w-screen max-sm:h-fit max-sm:py-5 h-20 z-2 bg-transparent flex max-sm:flex-col max-sm:gap-4 justify-between items-center px-8 backdrop-blur-sm fixed top-0 left-0">
             <div>
-                <Link href={"/"} className={`${abril_fatface.className} md:underline decoration-wavy decoration-amber-500 text-3xl `} >NodeMonks</Link>
+                <Link href={"/"} className={`${abril_fatface.className} underline decoration-wavy decoration-amber-500 text-3xl `} >NodeMonks</Link>
             </div>
-            <div className="w-fit flex gap-4 justify-between items-center">
+            <div className="max-sm:hidden w-fit flex gap-4 justify-between items-center">
                 <Link href={'#work'} className="bg-zinc-800 transition-all duration-100 hover:bg-white  hover:text-black py-1 px-4 rounded-full" >Work</Link>
                 <Link href={"#about"} className="bg-zinc-800 transition-all duration-100 hover:bg-white  hover:text-black py-1 px-4 rounded-full" >About Us</Link>
                 <Link href={"#contact"} className="bg-zinc-800 transition-all duration-100 hover:bg-white  hover:text-black py-1 px-4 rounded-full">Contact Us</Link>
